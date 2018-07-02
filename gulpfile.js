4572c747d504f79569737c59fa15d9d1b17d98de
Zeljko Galetic <zeljkogaletic@gmail.com>
2018-07-02 20:25:43 +0200

    Completely re-written grid mixins

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
 const glob = require('glob');
+const vinylfs = require('vinyl-fs');
 const argv = require('yargs').argv;
 
 gulp.task('dist', [], () => {
-  return gulp.src(['./src/*.scss'])
+  return vinylfs.src(['./src/*.scss', '!.src/tests.scss'])
     .pipe(sass({
       'outputStyle': argv.dev ? 'development' : 'compressed'
     }).on('error', sass.logError))
@@ -13,7 +15,17 @@ gulp.task('dist', [], () => {
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['dist'], () => {});
+gulp.task('tests', [], () => {
+  return gulp.src(['./src/tests.scss'])
+    .pipe(sass({
+      'outputStyle': argv.dev ? 'development' : 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./examples/'));
+});
+
+gulp.task('default', ['dist', 'tests'], () => {});
 
 gulp.task('watch', ['default'], () => {
   gulp.watch(glob.sync('./src/*.scss'), ['dist']);
