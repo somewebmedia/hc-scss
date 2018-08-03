3ca40ce210ec4f34c1d65d28e2d06d2ab65e8afa
Zeljko Galetic <zeljkogaletic@gmail.com>
2018-08-03 14:59:14 +0200

    Highlighter for the demos

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,22 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const glob = require('glob');
-const vinylfs = require('vinyl-fs');
 const argv = require('yargs').argv;
 
-gulp.task('dist', [], () => {
-  return vinylfs.src(['./src/*.scss', '!./src/tests.scss'])
+gulp.task('dist', () => {
+  return gulp.src(['./src/*.scss', '!./src/demo.scss'])
     .pipe(sass({
+      'includePaths': ['node_modules'],
       'outputStyle': argv.dev ? 'development' : 'compressed'
     }).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('tests', [], () => {
-  return gulp.src(['./src/tests.scss'])
+gulp.task('demo', () => {
+  return gulp.src(['./src/demo.scss'])
     .pipe(sass({
+      'includePaths': ['node_modules'],
       'outputStyle': argv.dev ? 'development' : 'compressed'
     }).on('error', sass.logError))
     .pipe(autoprefixer())
@@ -25,7 +26,7 @@ gulp.task('tests', [], () => {
     .pipe(gulp.dest('./examples/'));
 });
 
-gulp.task('default', ['dist', 'tests'], () => {});
+gulp.task('default', ['dist', 'demo'], () => {});
 
 gulp.task('watch', ['default'], () => {
   gulp.watch(glob.sync('./src/*.scss'), ['dist']);
